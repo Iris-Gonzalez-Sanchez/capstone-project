7982d984ba3a8ebbfb5b04a8634f1c97feddfa4f
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-09-13 20:00:20 -0400

    Restructered Home .js & App .js .css, Removed Navigation.js(css for navigation still in app.css)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-// import "./Home/Home.css";
-
 import SignUp from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import UserPage from "./components/Userpage/UserPage";
-import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(false);
@@ -24,18 +21,33 @@ function App() {
 
   const updateUser = (user) => setCurrentUser(user);
 
+  const handleLogOut = () => {
+    fetch("/logout", {
+      method: "DELETE",
+    });
+    updateUser(false)
+  };
+
   // if (errors) return <h1>{errors}</h1>;
 
   return (
     <>
-      <h1 className="project-title"> Libertas Token </h1>
-
-      <Navigation currentUser={currentUser} updateUser={updateUser} />
+ <div className="project-title"> Libertas Token </div>
+      {currentUser ? (
+        <div className="NavWithUser">
+          <a href="/">Home</a>
+          <a href={`/users/${currentUser.id}`} >User Page</a>
+          <a href="/" onClick={handleLogOut}>Logout</a>
+        </div>
+      ) : (
+        <div className="NavWithoutUser">
+          <a href="/">Home</a>
+          <a href="/login">Sign in</a>
+          <a href="/signup">Sign up</a>
+        </div>
+      )}
       <Routes>
-
-
         <Route path="/signup" element={<SignUp updateUser={updateUser} />} />
-
         <Route path="/login" element={<Login updateUser={updateUser} />} />
 
         {currentUser ? (
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,25 +6,25 @@ function Home({ user }) {
       return <h1>Welcome back, {user.username}!</h1>;
     } else {
       return<>
-            <div className="homepage-container">
-              <div className="section">
-           
+           <div>
+            <div>
+
                 <h2>✨Libertas Token, the freedom to learn. Use this site to learn stock behaviors and predictions. ✨</h2>
               </div>
 
-              <div className="section-bg1">
-                <h3 className="reveal-up">How to use this site:</h3>
-                <h3 className="reveal-up">Sign in or if new user, Sign up!</h3> 
-                <h3 className="reveal-up">Click the Stock tab above to add stocks you'd like to track onto your UserPage 😄</h3>
-                <h4 className="reveal-up"> Happy Tracking! * Insert Image / Gif of Features to Expect Here*</h4>
+              <div>
+                <h3>How to use this site:</h3>
+                <h3>Sign in or if new user, Sign up!</h3> 
+                <h3>Click the Stock tab above to add stocks you'd like to track onto your UserPage 😄</h3>
+                <h4> Happy Tracking! * Insert Image / Gif of Features to Expect Here*</h4>
               </div>
 
-              <div className="section-bg2">
-                <h4 className="reveal-up">✨✨Libertas Token, Track Stocks with Ease✨✨</h4>
+              <div>
+                <h4>✨✨Libertas Token, Track Stocks with Ease✨✨</h4>
               </div>
-              <div className="spacer">
+              <div >
                 <h4>Created by: Iris Gonzalez Sanchez</h4>
-              </div>
+            </div>
             </div>
             </>
     }
